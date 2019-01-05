b87c22588409a893a80cc223101f5e6a6046d4d6
Ghost1499 <zgstv@yandex.ru>
2019-01-05 16:36:08 +0300

    1

---
+++ Task7/Data/BalanceOfPower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class BalanceOfPower : IIndicator
+    {
+        public BalanceOfPower()
+        { }
+
+        public decimal Calculate(Candle candle) => (candle.Close - candle.Open) / (candle.High - candle.Low) * 100;
+    }
+}
